75b3b2f78502a77ba1f7053ea2b1233333fb5f08
Julie Nisbet <jmnisbet@gmail.com>
2019-10-21 15:53:14 -0700

    reorganizing things

--- hello_world.py
+++ hello_world.py
@@ -1,6 +1,1 @@
-def string_counter(string_1, string_2='a'):
-    return string_1.count(string_2)
-
-print(string_counter("aaaaa"))
-
-
+print("hello world!")
---
+++ lessons/week-3/week-3.py
@@ -0,0 +1,5 @@
+def shout(message, punc="!"):
+  print(message.upper() + punc)
+
+shout("hello")
+shout("how are you", punc="?")
