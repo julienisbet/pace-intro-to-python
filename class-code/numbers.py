c94ff014c715a8b1c7f3a38226a1f16191ff5fe3
Julie Nisbet <jmnisbet@gmail.com>
2020-01-16 15:42:22 -0800

    Week 1 class code

---
+++ class-code/numbers.py
@@ -0,0 +1,32 @@
+# Lets open up the Python shell in IDLE and play around with numbers.
+# The first type of number we'll work with is integers
+1
+10
+4382780
+# Integers are just numbers that don’t have a decimal point.
+# You can do basic calculations such as addition calculations
+1 + 2
+3 + 2
+3 - 2
+3 * 2
+3 / 2
+# floats
+4 / 2 # also returns a float
+4 + 2.0 # returns a float
+
+## Integer division
+
+5 // 3
+
+## Modulo
+
+5 % 3
+
+# converting strings to integers and back
+type(1234)
+type(1234.5)
+type(input())
+
+a = int(input(a))
+b = str(1)
+print(type(b))
