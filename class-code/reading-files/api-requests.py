6979c85e20e2231e6bbd03db313a7669014141d2
Julie Nisbet <jmnisbet@gmail.com>
2020-03-05 15:41:40 -0800

    Update api-requests.py
    add countries API

--- class-code/reading-files/api-requests.py
+++ class-code/reading-files/api-requests.py
@@ -1,13 +1,18 @@
 # http://api.openweathermap.org/data/2.5/weather?q=Goleta&APPID=bc406a21e03e694e8d239386b3b83d53
 # python3 -m pip install --user requests
-import requests, json
-API_KEY = "YOUR KEY HERE"
-BASE_URL = "http://api.openweathermap.org/data/2.5/weather?"
+# import requests, json
+# API_KEY = "YOUR KEY HERE"
+# BASE_URL = "http://api.openweathermap.org/data/2.5/weather?"
 
-city_name = 'Goleta'
-complete_url = BASE_URL + "appid=" + API_KEY + "&q=" + city_name
-response = requests.get(complete_url)
-data = response.json()
+# city_name = 'Goleta'
+# complete_url = BASE_URL + "appid=" + API_KEY + "&q=" + city_name
+# response = requests.get(complete_url)
+# data = response.json()
 
+import requests
 
+url = "https://restcountries.eu/rest/v2/name/united%20states%20of%20america"
 
+response = requests.get(url)
+
+print(response.json()[0]['name'])
